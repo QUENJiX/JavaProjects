a35a1d7a5f1dd8e0492bd586ad6744a1c3c3e381
QUENJiX <quasarskingdom@gmail.com>
2026-04-16 07:19:56 +0600

    Refactor Main class: streamline input handling and enhance name removal functionality; add VectorDemo class for demonstration

--- CSE215L/src/practice/p01_basics/VectorDemo.java
+++ CSE215L/src/practice/p01_basics/VectorDemo.java
@@ -1,13 +1,26 @@
+package practice.p01_basics;
+
 import java.util.Scanner;
 import java.util.Vector;
+/*
+ * Vector Functions:
+ * add() - adds an element to the end of the vector
+ * get() - retrieves the element at a specified index
+ * size() - returns the number of elements in the vector
+ * remove() - removes the element at a specified index
+ * clear() - removes all elements from the vector
+ * contains() - checks if the vector contains a specified element
+ * isEmpty() - checks if the vector is empty
+ */
 
-public class Main {
+public class VectorDemo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Vector<String> names = new Vector<>();
-        
+
         System.out.print("Enter the number of names: ");
         int n = input.nextInt();
+
         for (int i = 0; i < n; i++) {
             System.out.print("Enter name " + (i + 1) + ": ");
             String name = input.next();
